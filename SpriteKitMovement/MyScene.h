9f8c1b8481e382c014a397cb1977d74ba3bea6c6
Scott Moore <scott@buffbit.com>
2013-12-07 21:15:32 -0500

    Initial Commit

---
+++ SpriteKitMovement/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitMovement
+//
+
+//  Copyright (c) 2013 Buffbit. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ SpriteKitMovement/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpriteKitMovement
+//
+//  Created by oberonix on 12/7/13.
+//  Copyright (c) 2013 Buffbit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
